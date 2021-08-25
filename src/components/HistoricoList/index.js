c41c0d85a0e5722c27923471702103e680d290e5
Matheus Barboza <matheus.barboza102@gmail.com>
2021-08-25 16:46:41 -0300

    Add function to swipeable to delete

--- src/components/HistoricoList/index.js
+++ src/components/HistoricoList/index.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import { View, Text, Animated } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 import { Container, Tipo, IconView, TipoText, ValorText } from './styles';
 
 export default function HistoricoList({ data, deleteItem }) {
-  console.log(data);
+
+
+  function RenderRight(progress, dragX) {
+
+    const scale = dragX.interpolate({
+      inputRange: [-50, 0.5],
+      outputRange: [1, 0.1]
+    })
+
+    const Style = {
+      transform: [
+        {
+          scale
+        }
+      ]
+    }
+
+    return (
+      <View style={{ width: 75, height: 77, backgroundColor: 'red', alignItems: 'center', justifyContent: 'center' }}>
+        <Animated.Text style={[Style, { color: '#fff', fontWeight: '600' }]}>
+          <Icon
+            name={'trash'}
+            color='#fff'
+            size={20} />
+        </Animated.Text>
+      </View>
+    );
+  }
+
   return (
-    <TouchableWithoutFeedback onLongPress={() => deleteItem(data)} >
+    <Swipeable
+      useNativeAnimations overshootRight={false}
+      onSwipeableRightOpen={() => deleteItem(data)}
+      renderRightActions={RenderRight}
+    >
       <Container>
         <Tipo>
           <IconView tipo={data.tipo} >
@@ -21,6 +55,6 @@ export default function HistoricoList({ data, deleteItem }) {
 
         <ValorText>R$ {data.valor.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1. ')}</ValorText>
       </Container>
-    </TouchableWithoutFeedback>
+    </Swipeable>
   );
 }
